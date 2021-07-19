82508f828a07588a54ac812410f7ad58388f6055
diego.felix <diego.felix@diegofelixs-MacBook-Pro.local>
2021-07-18 23:35:40 -0300

    refactor: add controller interface

--- src/presentation/controllers/add-partner-controller.ts
+++ src/presentation/controllers/add-partner-controller.ts
@@ -1,8 +1,9 @@
+import { Controller } from './controller'
 import { MissingField } from './helpers/errors/missign-field'
 import { HttpRequest, HttpResponse } from './helpers/http/http'
 import { badRequest } from './helpers/http/http-response-status-code'
 
-export class AddPartnerController {
+export class AddPartnerController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const fields: Array<string> = ['tradingName', 'ownerName', 'document', 'coverageArea', 'address']
     for (const field of fields) {
